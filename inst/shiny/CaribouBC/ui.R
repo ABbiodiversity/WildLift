9260220d1d0563ec2861b587f184f2ab7dae6dde
Peter Solymos <psolymos@gmail.com>
2020-05-11 16:26:29 -0600

    tooltip for wolf slider

--- inst/shiny/CaribouBC/ui.R
+++ inst/shiny/CaribouBC/ui.R
@@ -250,7 +250,10 @@ dashboardPage(
                 "Select a herd for herd specific demography parameters.",
                 placement="top"),
               sliderInput("wolf_nremove", "Number of wolfs to remove",
-                min = 0, max = 200, value = 0, step = 1)
+                min = 0, max = 200, value = 0, step = 1),
+              bsTooltip("wolf_nremove",
+                "The wolf slider does not change outcomes because it assumes users are entering the optimal number of wolves to be removed (i.e., enough wolves to achieve 2/1000 sq km). The slider is used only to calculate costs.",
+                placement="left"),
             ),
             box(
               width = NULL, status = "info", solidHeader = TRUE,
