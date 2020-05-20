204bbcde37da4a40e37338fafe598393b73532d7
Peter Solymos <psolymos@gmail.com>
2020-05-20 00:04:37 -0600

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+SHA=$(git log -1 --format=%h)
+URL=$(grep url .git/config | cut -d' ' -f3 | head -1)
+VER=$(awk '/^Version:/ {print $2}' DESCRIPTION)
+PKG=$(awk '/^Package:/ {print $2}' DESCRIPTION)
+
 cd ..
-R CMD build WildLift
-PKGNAME=$(ls -1t *.tar.gz | head -n 1)
+R CMD build ${PKG}
 
 git clone -b gh-pages https://github.com/ABbiodiversity/drat.git _tmpdrat
-Rscript -e "drat::insertPackage('${PKGNAME}', '_tmpdrat')"
+Rscript -e "drat::insertPackage('${PKG}_${VER}.tar.gz', '_tmpdrat')"
 cd _tmpdrat
 git add --all *
-git commit -m "Update ${PKGNAME}"
+git commit -m "${PKG} ${VER} from ${URL} ${SHA}"
 git push -q origin gh-pages
 
+cd ..
+rm -rf _tmpdrat
+cd ${PKG}
